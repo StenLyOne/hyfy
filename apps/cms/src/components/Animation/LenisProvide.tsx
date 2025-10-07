087b21890c82478ab57dd6493b6488e78acf183b
Vlad <macm3@macs-MacBook-Pro.local>
2025-10-07 07:38:37 -0700

    fixed deploy error of lenis

--- apps/cms/src/components/Animation/LenisProvide.tsx
+++ apps/cms/src/components/Animation/LenisProvide.tsx
@@ -12,8 +12,8 @@ export default function LenisProvider({ children }: { children: ReactNode }) {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      smoothTouch: false,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
     });
 
     lenisRef.current = lenis;
